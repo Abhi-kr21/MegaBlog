238fa82a04796592a14e9ae3853868450bf47af3
Abhi-kr21 <abhishek7858010345@gmail.com>
2024-02-09 17:07:59 +0530

    megaBlog

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import viteLogo from '/vite.svg'
+function Logo({width = '100px'}) {
+  return (
+    <img src='/vite.svg' style={{width}} alt='Logo placeholder' />
+  )
+}
+
+export default Logo
